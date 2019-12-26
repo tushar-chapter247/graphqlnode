508b5a37eeeb78f4414eede8c2aa2bbf5d9502d7
Tushar Goyal <tushar.chapter247@gmail.com>
2019-12-26 16:39:15 +0530

    Initialize project and configure sequelize postgresql and graphql playground

---
+++ graphql/schema/index.js
@@ -0,0 +1,53 @@
+import { gql } from 'apollo-server-express';
+
+const typeDefs = gql`
+	type Rocket {
+		id: ID!
+		name: String
+		type: String
+	}
+
+	type Mission {
+		name: String
+		missionPatch(size: PatchSize): String
+	}
+
+	enum PatchSize {
+		SMALL
+		LARGE
+	}
+
+	type Launch {
+		id: ID!
+		site: String
+		mission: Mission
+		rocket: Rocket
+		isBooked: Boolean!
+	}
+
+	type User {
+		id: ID!
+		email: String!
+		trips: [Launch]!
+	}
+
+	type Query {
+		launches: [Launch]!
+		launch(id: ID!): Launch
+		me: User
+	}
+
+	type Mutation {
+		bookTrips(launchIds: [ID]!): TripUpdateResponse!
+		cancelTrip(launchId: ID!): TripUpdateResponse!
+		login(email: String): String
+	}
+
+	type TripUpdateResponse {
+		success: Boolean!
+		message: String
+		launches: [Launch]
+	}
+`;
+
+module.exports = typeDefs;
